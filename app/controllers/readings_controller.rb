f045d7d50cb49a93ef1c63f884910689402ddc14
railskipl <rails@kunalinfotech.net>
2013-08-23 14:40:53 +0530

    image added

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,6 +1,5 @@
 class ReadingsController < ApplicationController
 
- before_filter  :authenticate ,:only => [:edit,:update,:index]
 
 
   # GET /dtc_staffs
@@ -31,19 +30,17 @@ class ReadingsController < ApplicationController
   # POST /dtc_staffs
   # POST /dtc_staffs.json
   def create
-    if params[:device] == 'mobile' 
+   
+    
 
      
-
-   @reading = Reading.create(:meter_reading=>params[:meter_reading],:consumer_no=>params[:consumer_no],:pc =>params[:pc],:pincode =>params[:pincode],:bu =>params[:bu],:address=>params[:address],:city=>params[:city],:consumer_name=> params[:consumer_name],:dtc =>params[:dtc],:pole_no =>params[:pole_no],:reader_mobile_no=>params[:reader_mobile_no],:date_time=>params[:date_time],:latitude=>params[:latitude],:longitude =>params[:longitude],:ime_no=>params[:ime_no],:image=>params[:image],
+   @reading = Reading.create(:meter_reading=>params[:meter_reading],:consumer_no=>params[:consumer_no],:pc =>params[:pc],:pincode =>params[:pincode],:bu =>params[:bu],:address=>params[:address],:city=>params[:city],:consumer_name=> params[:consumer_name],:dtc =>params[:dtc],:pole_no =>params[:poleno],:reader_mobile_no=>params[:reader_mobile_no],:date_time=>params[:date_time],:latitude=>params[:latitude],:longitude =>params[:longitude],:ime_no=>params[:ime_no],:image=>params[:uploaded],
     :meter_status=>params[:meter_status],:old_meter_no=>params[:old_meter_no],:new_meter_no=>params[:new_meter_no],:bill_month=>params[:bill_month],:meter_reader_status=>params[:meter_reader_status],:remark=>params[:remark],:read_by=>params[:read_by])
    
 
        render :status =>200,:json => { :error => "valid" } 
-      else
-        render :status =>401,:json => { :error => "Invalid" } 
-      end
    
+      
     
   end
 

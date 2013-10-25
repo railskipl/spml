717942fd7ff724084b7b030e04556b85adf74cdf
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-10-25 13:31:13 +0530

    file removed

--- db/migrate/20130821114432_add_details_to_readings.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToReadings < ActiveRecord::Migration
-  def change
-    add_column :readings, :pincode, :integer
-    add_column :readings, :address, :string
-    add_column :readings, :city, :string
-    
-  end
-end

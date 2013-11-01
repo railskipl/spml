da938cc6a72c0937039d8766b552080b25ad0fb1
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-11-01 13:25:30 +0530

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_filter :authenticate, :only => [:edit, :update]
+    before_filter :authenticate, :only => [:edit, :update,:dashboard]
     before_filter :correct_user, :only => [:edit, :update]
 
     

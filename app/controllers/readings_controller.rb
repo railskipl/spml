3c9d19a6be0b7ad346bf614e979a46bae56e45db
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-11-11 13:26:30 +0530

    date range filter added

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -186,10 +186,18 @@ class ReadingsController < ApplicationController
     start_to = "#{params['end_date']} #{params['timepicker2']}"
 
     if start_from.blank? || start_to.blank?
-     redirect_to activity_report_readings_url, alert: "Please select date and time"
+      if  params["reading_index"]
+       redirect_to readings_path , alert: "Please select date and time"
+      else
+       redirect_to activity_report_readings_url, alert: "Please select date and time"
+      end
     else
     if start_from > start_to
+     if params["reading_index"]
+      redirect_to readings_path , alert: "Start time cannot be greate"
+     else
      redirect_to activity_report_readings_url, alert: "Start time cannot be greater"
+     end
     else
     @readings = Reading.where("date_time >= ? and date_time <= ?" ,start_from,start_to)
     end

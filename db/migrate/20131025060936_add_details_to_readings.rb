666a9408996ca0b1f7028bfd37c5ac77c2911089
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-01-11 13:32:14 +0530

    files removed

--- db/migrate/20131025060936_add_details_to_readings.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToReadings < ActiveRecord::Migration
-  def change
-    add_column :readings, :pincode, :string
-    add_column :readings, :address, :string
-    add_column :readings, :city, :string
-  end
-end
--- db/migrate/20131028102124_add_details_to_dtc_staffs.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToDtcStaffs < ActiveRecord::Migration
-  def change
-    add_column :dtc_staffs, :bu, :string
-    add_column :dtc_staffs, :pc, :string
-    add_column :dtc_staffs, :route_name, :string
-  end
-end
